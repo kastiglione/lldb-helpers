2dd284bab3007b8e69a61981cc0e641b942025bb
Dave Lee <davelee.com@gmail.com>
2018-06-10 21:49:24 -0700

    Add called_on for thread (and queue) criteria

--- criteria.py
+++ criteria.py
@@ -65,3 +65,12 @@ def any_caller_from(name):
         return any(f.module.file.basename == name for f in callers)
     return check
 __builtin__.any_caller_from = any_caller_from
+
+# breakpoint command add -F 'called_on(1)'
+# breakpoint command add -F 'not called_on("thisThread")'
+def called_on(identifier):
+    if isinstance(identifier, int):
+        return lambda frame, loc, _: identifier == frame.thread.idx
+    else:
+        return lambda frame, loc, _: identifier in [frame.thread.name, frame.thread.queue]
+__builtin__.called_on = called_on
